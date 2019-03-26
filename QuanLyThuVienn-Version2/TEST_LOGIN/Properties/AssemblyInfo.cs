94ed2d92103013aece2d62a63aa76b2f07004e9b
Thien An <thienan2497@gmail.com>
2019-03-26 17:02:08 +0700

    Chinh sua duong dan vao DB

---
+++ QuanLyThuVienn-Version2/TEST_LOGIN/Properties/AssemblyInfo.cs
@@ -0,0 +1,20 @@
+using System.Reflection;
+using System.Runtime.CompilerServices;
+using System.Runtime.InteropServices;
+
+[assembly: AssemblyTitle("TEST_LOGIN")]
+[assembly: AssemblyDescription("")]
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyCompany("")]
+[assembly: AssemblyProduct("TEST_LOGIN")]
+[assembly: AssemblyCopyright("Copyright ©  2019")]
+[assembly: AssemblyTrademark("")]
+[assembly: AssemblyCulture("")]
+
+[assembly: ComVisible(false)]
+
+[assembly: Guid("cc5b6994-15c0-4ac2-bc9c-bbdbd68c02f4")]
+
+// [assembly: AssemblyVersion("1.0.*")]
+[assembly: AssemblyVersion("1.0.0.0")]
+[assembly: AssemblyFileVersion("1.0.0.0")]
---
+++ QuanLyThuVienn-Version2/TEST_LOGIN/UnitTest1.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using QuanLyThuVien;
+namespace TEST_LOGIN
+{
+    [TestClass]
+    public class UnitTest1
+    {
+        private Main main;
+        [TestMethod]
+        public void Test_Nhap_Dung_Tai_Khoan()
+        {
+             this.
+        }
+    }
+}
